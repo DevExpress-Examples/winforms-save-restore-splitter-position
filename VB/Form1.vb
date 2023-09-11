78a09c2581d85f8b3c2ac45e5c9c317e43e9ef29
DevExpressExampleBot <fakeEmail>
2023-09-11 22:20:45 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,153 +1,154 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Drawing
-Imports System.Collections
 Imports System.ComponentModel
 Imports System.Windows.Forms
-Imports System.Data
 
 Namespace SaveRestoreSplitterPosition
-	''' <summary>
-	''' Summary description for Form1.
-	''' </summary>
-	Public Class Form1
-		Inherits System.Windows.Forms.Form
-		Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
-		Private WithEvents simpleButton2 As DevExpress.XtraEditors.SimpleButton
-		Private panel1 As System.Windows.Forms.Panel
-		Private BasePanel As System.Windows.Forms.Panel
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.Container = Nothing
 
-		Public Sub New()
-			'
-			' Required for Windows Form Designer support
-			'
-			InitializeComponent()
+    ''' <summary>
+    ''' Summary description for Form1.
+    ''' </summary>
+    Public Class Form1
+        Inherits Form
 
-			'
-			' TODO: Add any constructor code after InitializeComponent call
-			'
-		End Sub
+        Private simpleButton1 As DevExpress.XtraEditors.SimpleButton
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		Protected Overrides Overloads Sub Dispose(ByVal disposing As Boolean)
-			If disposing Then
-				If components IsNot Nothing Then
-					components.Dispose()
-				End If
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        Private simpleButton2 As DevExpress.XtraEditors.SimpleButton
 
-		#Region "Windows Form Designer generated code"
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
-			Me.simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
-			Me.panel1 = New System.Windows.Forms.Panel()
-			Me.BasePanel = New System.Windows.Forms.Panel()
-			Me.panel1.SuspendLayout()
-			Me.SuspendLayout()
-			' 
-			' simpleButton1
-			' 
-			Me.simpleButton1.Location = New System.Drawing.Point(16, 8)
-			Me.simpleButton1.Name = "simpleButton1"
-			Me.simpleButton1.Size = New System.Drawing.Size(168, 23)
-			Me.simpleButton1.TabIndex = 0
-			Me.simpleButton1.Text = "Load module"
-'			Me.simpleButton1.Click += New System.EventHandler(Me.simpleButton1_Click);
-			' 
-			' simpleButton2
-			' 
-			Me.simpleButton2.Location = New System.Drawing.Point(192, 8)
-			Me.simpleButton2.Name = "simpleButton2"
-			Me.simpleButton2.Size = New System.Drawing.Size(168, 23)
-			Me.simpleButton2.TabIndex = 1
-			Me.simpleButton2.Text = "Unload module"
-'			Me.simpleButton2.Click += New System.EventHandler(Me.simpleButton2_Click);
-			' 
-			' panel1
-			' 
-			Me.panel1.Controls.Add(Me.simpleButton2)
-			Me.panel1.Controls.Add(Me.simpleButton1)
-			Me.panel1.Dock = System.Windows.Forms.DockStyle.Top
-			Me.panel1.Location = New System.Drawing.Point(0, 0)
-			Me.panel1.Name = "panel1"
-			Me.panel1.Size = New System.Drawing.Size(504, 48)
-			Me.panel1.TabIndex = 2
-			' 
-			' BasePanel
-			' 
-			Me.BasePanel.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.BasePanel.Location = New System.Drawing.Point(0, 48)
-			Me.BasePanel.Name = "BasePanel"
-			Me.BasePanel.Size = New System.Drawing.Size(504, 282)
-			Me.BasePanel.TabIndex = 3
-			' 
-			' Form1
-			' 
-			Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
-			Me.ClientSize = New System.Drawing.Size(504, 330)
-			Me.Controls.Add(Me.BasePanel)
-			Me.Controls.Add(Me.panel1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			Me.panel1.ResumeLayout(False)
-			Me.ResumeLayout(False)
+        Private panel1 As Panel
 
-		End Sub
-		#End Region
+        Private BasePanel As Panel
 
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread> _
-		Shared Sub Main()
-			Application.Run(New Form1())
-		End Sub
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.Container = Nothing
 
-		Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles simpleButton1.Click
-			CurrentModule = New UserControl1()
-		End Sub
+        Public Sub New()
+            '
+            ' Required for Windows Form Designer support
+            '
+            InitializeComponent()
+        '
+        ' TODO: Add any constructor code after InitializeComponent call
+        '
+        End Sub
 
-		Private Sub simpleButton2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles simpleButton2.Click
-			CurrentModule = Nothing
-		End Sub
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing Then
+                If components IsNot Nothing Then
+                    components.Dispose()
+                End If
+            End If
 
-		Private currentModule_Renamed As UserControl
-		Private Property CurrentModule() As UserControl
-			Get
-				Return currentModule_Renamed
-			End Get
-			Set(ByVal value As UserControl)
-				If currentModule_Renamed IsNot value Then
-					If currentModule_Renamed IsNot Nothing Then
-						UnloadModule(currentModule_Renamed)
-					End If
-					If value IsNot Nothing Then
-						LoadModule(value)
-					End If
-					currentModule_Renamed = value
-				End If
-			End Set
-		End Property
+            MyBase.Dispose(disposing)
+        End Sub
 
-		Private Sub LoadModule(ByVal [module] As UserControl)
-			[module].Dock = DockStyle.Fill
-			BasePanel.Controls.Add([module])
-		End Sub
-		Private Sub UnloadModule(ByVal [module] As UserControl)
-			BasePanel.Controls.Remove([module])
-			[module].Dispose()
-		End Sub
-	End Class
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+            simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+            panel1 = New Panel()
+            BasePanel = New Panel()
+            panel1.SuspendLayout()
+            Me.SuspendLayout()
+            ' 
+            ' simpleButton1
+            ' 
+            simpleButton1.Location = New System.Drawing.Point(16, 8)
+            simpleButton1.Name = "simpleButton1"
+            simpleButton1.Size = New System.Drawing.Size(168, 23)
+            simpleButton1.TabIndex = 0
+            simpleButton1.Text = "Load module"
+            AddHandler simpleButton1.Click, New EventHandler(AddressOf simpleButton1_Click)
+            ' 
+            ' simpleButton2
+            ' 
+            simpleButton2.Location = New System.Drawing.Point(192, 8)
+            simpleButton2.Name = "simpleButton2"
+            simpleButton2.Size = New System.Drawing.Size(168, 23)
+            simpleButton2.TabIndex = 1
+            simpleButton2.Text = "Unload module"
+            AddHandler simpleButton2.Click, New EventHandler(AddressOf simpleButton2_Click)
+            ' 
+            ' panel1
+            ' 
+            panel1.Controls.Add(simpleButton2)
+            panel1.Controls.Add(simpleButton1)
+            panel1.Dock = DockStyle.Top
+            panel1.Location = New System.Drawing.Point(0, 0)
+            panel1.Name = "panel1"
+            panel1.Size = New System.Drawing.Size(504, 48)
+            panel1.TabIndex = 2
+            ' 
+            ' BasePanel
+            ' 
+            BasePanel.Dock = DockStyle.Fill
+            BasePanel.Location = New System.Drawing.Point(0, 48)
+            BasePanel.Name = "BasePanel"
+            BasePanel.Size = New System.Drawing.Size(504, 282)
+            BasePanel.TabIndex = 3
+            ' 
+            ' Form1
+            ' 
+            AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+            ClientSize = New System.Drawing.Size(504, 330)
+            Me.Controls.Add(BasePanel)
+            Me.Controls.Add(panel1)
+            Name = "Form1"
+            Text = "Form1"
+            panel1.ResumeLayout(False)
+            Me.ResumeLayout(False)
+        End Sub
+
+#End Region
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Shared Sub Main()
+            Call Application.Run(New Form1())
+        End Sub
+
+        Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            CurrentModule = New UserControl1()
+        End Sub
+
+        Private Sub simpleButton2_Click(ByVal sender As Object, ByVal e As EventArgs)
+            CurrentModule = Nothing
+        End Sub
+
+        Private currentModuleField As UserControl
+
+        Private Property CurrentModule As UserControl
+            Get
+                Return currentModuleField
+            End Get
+
+            Set(ByVal value As UserControl)
+                If currentModuleField IsNot value Then
+                    If currentModuleField IsNot Nothing Then UnloadModule(currentModuleField)
+                    If value IsNot Nothing Then LoadModule(value)
+                    currentModuleField = value
+                End If
+            End Set
+        End Property
+
+        Private Sub LoadModule(ByVal [module] As UserControl)
+            [module].Dock = DockStyle.Fill
+            BasePanel.Controls.Add([module])
+        End Sub
+
+        Private Sub UnloadModule(ByVal [module] As UserControl)
+            BasePanel.Controls.Remove([module])
+            [module].Dispose()
+        End Sub
+    End Class
 End Namespace
