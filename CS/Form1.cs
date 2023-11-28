c3bf1d66c16e295d96c92b40f6536a232e9089f2
Norton <norton@theleftexit.net>
2023-11-28 15:22:25 +0400

    Reworked to use app settings instead of BinaryFormatter

--- CS/Form1.cs
+++ CS/Form1.cs
@@ -5,151 +5,144 @@ using System.ComponentModel;
 using System.Windows.Forms;
 using System.Data;
 
-namespace SaveRestoreSplitterPosition
-{
-	/// <summary>
-	/// Summary description for Form1.
-	/// </summary>
-	public class Form1 : System.Windows.Forms.Form
-	{
-		private DevExpress.XtraEditors.SimpleButton simpleButton1;
-		private DevExpress.XtraEditors.SimpleButton simpleButton2;
-		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.Panel BasePanel;
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
+namespace SaveRestoreSplitterPosition {
+    /// <summary>
+    /// Summary description for Form1.
+    /// </summary>
+    public class Form1 : System.Windows.Forms.Form {
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel BasePanel;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
 
-		public Form1()
-		{
-			//
-			// Required for Windows Form Designer support
-			//
-			InitializeComponent();
+        public Form1() {
+            //
+            // Required for Windows Form Designer support
+            //
+            InitializeComponent();
 
-			//
-			// TODO: Add any constructor code after InitializeComponent call
-			//
-		}
+            //
+            // TODO: Add any constructor code after InitializeComponent call
+            //
+        }
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if (components != null) 
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-			this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.BasePanel = new System.Windows.Forms.Panel();
-			this.panel1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// simpleButton1
-			// 
-			this.simpleButton1.Location = new System.Drawing.Point(16, 8);
-			this.simpleButton1.Name = "simpleButton1";
-			this.simpleButton1.Size = new System.Drawing.Size(168, 23);
-			this.simpleButton1.TabIndex = 0;
-			this.simpleButton1.Text = "Load module";
-			this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
-			// 
-			// simpleButton2
-			// 
-			this.simpleButton2.Location = new System.Drawing.Point(192, 8);
-			this.simpleButton2.Name = "simpleButton2";
-			this.simpleButton2.Size = new System.Drawing.Size(168, 23);
-			this.simpleButton2.TabIndex = 1;
-			this.simpleButton2.Text = "Unload module";
-			this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
-			// 
-			// panel1
-			// 
-			this.panel1.Controls.Add(this.simpleButton2);
-			this.panel1.Controls.Add(this.simpleButton1);
-			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-			this.panel1.Location = new System.Drawing.Point(0, 0);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(504, 48);
-			this.panel1.TabIndex = 2;
-			// 
-			// BasePanel
-			// 
-			this.BasePanel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.BasePanel.Location = new System.Drawing.Point(0, 48);
-			this.BasePanel.Name = "BasePanel";
-			this.BasePanel.Size = new System.Drawing.Size(504, 282);
-			this.BasePanel.TabIndex = 3;
-			// 
-			// Form1
-			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.ClientSize = new System.Drawing.Size(504, 330);
-			this.Controls.Add(this.BasePanel);
-			this.Controls.Add(this.panel1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.panel1.ResumeLayout(false);
-			this.ResumeLayout(false);
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BasePanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(16, 8);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(168, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Load module";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(192, 8);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(168, 23);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "Unload module";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.simpleButton2);
+            this.panel1.Controls.Add(this.simpleButton1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 48);
+            this.panel1.TabIndex = 2;
+            // 
+            // BasePanel
+            // 
+            this.BasePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BasePanel.Location = new System.Drawing.Point(0, 48);
+            this.BasePanel.Name = "BasePanel";
+            this.BasePanel.Size = new System.Drawing.Size(504, 282);
+            this.BasePanel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(504, 330);
+            this.Controls.Add(this.BasePanel);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
 
-		}
-		#endregion
+        }
+        #endregion
 
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main() 
-		{
-			Application.Run(new Form1());
-		}
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.Run(new Form1());
+        }
 
-		private void simpleButton1_Click(object sender, System.EventArgs e) {
-			CurrentModule = new UserControl1();		
-		}
+        private void simpleButton1_Click(object sender, System.EventArgs e) {
+            CurrentModule = new UserControl1();
+        }
 
-		private void simpleButton2_Click(object sender, System.EventArgs e) {
-			CurrentModule = null;
-		}
+        private void simpleButton2_Click(object sender, System.EventArgs e) {
+            CurrentModule = null;
+        }
 
-		UserControl currentModule;
-		UserControl CurrentModule {
-			get { return currentModule; }
-			set { 
-				if(currentModule != value) {
-					if(currentModule != null)
-						UnloadModule(currentModule);
-					if(value != null)
-						LoadModule(value);
-					currentModule = value;
-				}
-			}				
-		}
+        UserControl currentModule;
+        UserControl CurrentModule {
+            get { return currentModule; }
+            set {
+                if (currentModule != value) {
+                    if (currentModule != null)
+                        UnloadModule(currentModule);
+                    if (value != null)
+                        LoadModule(value);
+                    currentModule = value;
+                }
+            }
+        }
 
-		void LoadModule(UserControl module) {
-			module.Dock = DockStyle.Fill;
-            BasePanel.Controls.Add(module);			
-		}
-		void UnloadModule(UserControl module) {
-			BasePanel.Controls.Remove(module);
-			module.Dispose();
-		}
-	}
+        void LoadModule(UserControl module) {
+            module.Dock = DockStyle.Fill;
+            BasePanel.Controls.Add(module);
+        }
+        void UnloadModule(UserControl module) {
+            BasePanel.Controls.Remove(module);
+            module.Dispose();
+            Settings.Default.Save();
+        }
+    }
 }
--- CS/UserControl1.cs
+++ CS/UserControl1.cs
@@ -8,127 +8,85 @@ using System.IO;
 using System.Runtime.Remoting;
 using System.Runtime.Serialization.Formatters.Binary;
 
-namespace SaveRestoreSplitterPosition
-{
-	/// <summary>
-	/// Summary description for UserControl1.
-	/// </summary>
-	public class UserControl1 : System.Windows.Forms.UserControl
-	{
-		private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
-		/// <summary> 
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
+namespace SaveRestoreSplitterPosition {
+    /// <summary>
+    /// Summary description for UserControl1.
+    /// </summary>
+    public class UserControl1 : System.Windows.Forms.UserControl {
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
 
-		public UserControl1()
-		{
-			// This call is required by the Windows.Forms Form Designer.
-			InitializeComponent();
+        public UserControl1() {
+            // This call is required by the Windows.Forms Form Designer.
+            InitializeComponent();
 
-			// TODO: Add any initialization after the InitializeComponent call
+            // TODO: Add any initialization after the InitializeComponent call
 
-		}
+        }
 
-		/// <summary> 
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				Unloading();
-				if(components != null)
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Component Designer generated code
-		/// <summary> 
-		/// Required method for Designer support - do not modify 
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
-			((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
-			this.splitContainerControl1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// splitContainerControl1
-			// 
-			this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainerControl1.Location = new System.Drawing.Point(0, 0);
-			this.splitContainerControl1.Name = "splitContainerControl1";
-			this.splitContainerControl1.Panel1.Text = "splitContainerControl1_Panel1";
-			this.splitContainerControl1.Panel2.Text = "splitContainerControl1_Panel2";
-			this.splitContainerControl1.Size = new System.Drawing.Size(256, 160);
-			this.splitContainerControl1.TabIndex = 0;
-			this.splitContainerControl1.Text = "splitContainerControl1";
-			// 
-			// UserControl1
-			// 
-			this.Controls.Add(this.splitContainerControl1);
-			this.Name = "UserControl1";
-			this.Size = new System.Drawing.Size(256, 160);
-			((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
-			this.splitContainerControl1.ResumeLayout(false);
-			this.ResumeLayout(false);
+        #region Component Designer generated code
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1.Panel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1.Panel2)).BeginInit();
+            this.splitContainerControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainerControl1
+            // 
+            this.splitContainerControl1.DataBindings.Add(new System.Windows.Forms.Binding("SplitterPosition", global::SaveRestoreSplitterPosition.Settings.Default, "SavedSplitterPosition", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl1.Name = "splitContainerControl1";
+            // 
+            // splitContainerControl1.Panel1
+            // 
+            this.splitContainerControl1.Panel1.Appearance.BackColor = System.Drawing.Color.Lime;
+            this.splitContainerControl1.Panel1.Appearance.Options.UseBackColor = true;
+            this.splitContainerControl1.Panel1.Text = "splitContainerControl1_Panel1";
+            // 
+            // splitContainerControl1.Panel2
+            // 
+            this.splitContainerControl1.Panel2.Appearance.BackColor = System.Drawing.Color.Red;
+            this.splitContainerControl1.Panel2.Appearance.Options.UseBackColor = true;
+            this.splitContainerControl1.Panel2.Text = "splitContainerControl1_Panel2";
+            this.splitContainerControl1.Size = new System.Drawing.Size(256, 160);
+            this.splitContainerControl1.SplitterPosition = global::SaveRestoreSplitterPosition.Settings.Default.SavedSplitterPosition;
+            this.splitContainerControl1.TabIndex = 0;
+            this.splitContainerControl1.Text = "splitContainerControl1";
+            // 
+            // UserControl1
+            // 
+            this.Controls.Add(this.splitContainerControl1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(256, 160);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1.Panel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1.Panel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
+            this.splitContainerControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
 
-		}
-		#endregion
-
-		protected override void OnLayout(LayoutEventArgs levent) {
-			base.OnLayout(levent);
-			if(levent.AffectedProperty == "Bounds")
-				Loaded();
-		}
-		bool isLoaded = false;
-		public virtual void Loaded() {
-			if(isLoaded) return;
-			isLoaded = true;
-			LoadSettings();
-		}
-		public virtual void Unloading() {
-			SaveSettings();
-		}
-
-		const string SettingsFile = "settings.dat";
-		public virtual void SaveSettings() {
-			Settings sets = new Settings();
-			sets.SplitterPosition = splitContainerControl1.SplitterPosition;
-            
-			FileStream stream = new FileStream(SettingsFile, FileMode.Create);
-			BinaryFormatter formatter = new BinaryFormatter();
-			formatter.AssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Simple;
-			formatter.Serialize(stream, sets);
-			stream.Close();
-
-			Console.WriteLine("Saved: {0}", sets.SplitterPosition);
-		}
-
-		public virtual void LoadSettings() {
-			if(!File.Exists(SettingsFile)) return;
-
-			FileStream stream = new FileStream(SettingsFile, FileMode.Open);
-			BinaryFormatter formatter = new BinaryFormatter();
-			formatter.AssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Simple;
-			Settings sets = formatter.Deserialize(stream) as Settings;
-			stream.Close();
-
-			if(sets != null) {
-				splitContainerControl1.SplitterPosition = sets.SplitterPosition;
-
-				Console.WriteLine("Loaded: {0}, New position: {1}", sets.SplitterPosition, splitContainerControl1.SplitterPosition);
-			}
-		}
-	}
-
-	[Serializable]
-	public class Settings {
-		public int SplitterPosition;
-	}
+        }
+        #endregion
+    }
 }
